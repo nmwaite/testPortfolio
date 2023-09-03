db9ade21c246eeacc588152d43bbf580d8b1e328
nmwaite <142501425+nmwaite@users.noreply.github.com>
2023-09-04 05:42:49 +0700

    Added another app called projects to display some example projects

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -19,8 +19,13 @@ from django.urls import path
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("pages.urls"))
-]
+    #path("", include("pages.urls")),
+    path("", include("projects.urls"))
+    #path("projects/", include("projects.urls"))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
